6feb2a04e4cf071a9472427316bb59252706724f
Renan <renanbezerraenes@icloud.com>
2022-01-14 01:43:38 +0100

    First exercise at Strive with JS, simple exercise.

---
+++ M2-D1/index.js
@@ -0,0 +1,21 @@
+let number1 = 12
+let number2 = 20
+let numbers = number1 + number2
+
+console.log(numbers)
+
+
+
+let x = 12
+
+let subtraction = x / 4
+console.log(subtraction)
+
+
+let name1 = "john"
+let name2 = "John"
+console.log(name1 === name2)
+
+console.log(name1.toLowerCase === name2.toLowerCase)
+
+
