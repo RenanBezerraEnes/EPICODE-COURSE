14f10f015826747900574e13602203ebdefc8948
Renan Bezerra <renanbezerraenes@icloud.com>
2022-03-16 21:21:41 +0100

    This project is done
    💻EXERCISES:
     1) When pressing on Load Images button, load the pictures from
    https://api.pexels.com/v1/search?query=[your-query]   2) When pressing on Load Seconday Images, load the pictures from
    https://api.pexels.com/v1/search?query=[your-secondary-query]
    
    3) The Edit button should be replaced with a "Hide" button.
    
    4) When the hide button is pressed, the whole picture card should disappear.
    
    5) Replace the "9 mins" string in the card template with the ID of the Image    6) Add in the "jumbotron" a search field. Use the value of the search field to search new images
    and replace the existing ones.
    
    💻EXTRAS:
    Handle API errors gracefully, using alert components with a custom message inside
    Add a carousel at the bottom of the page with images from another API call
    When the user clicks on the view button inside a card, open that image in a modal
    💻MORE EXTRAS:
    Use the .map method to create, from your API response, an array containing just the url strings (you can display the result with a console.log)
    Use the .filter method to modify the result of the API call. Keep only the images from a specific author or your chose (you can display the result with a console.log)

--- M4 - Week 2 - Part 1/index.js
+++ M4 - Week 2 - Part 1/index.js
@@ -1,7 +1,8 @@
 const loadImages = document.querySelector("#loadImages");
 const password = "563492ad6f9170000100000175681c8999154aaaac8307cf3d6bad89";
+const rowCards = document.querySelector("#row-cards");
 
-// First Button Load Images
+//****************************************** First Button Load Images *********************************************
 loadImages.addEventListener("click", () => {
   const url = "https://api.pexels.com/v1/search?query=computers";
 
@@ -14,20 +15,44 @@ loadImages.addEventListener("click", () => {
     .then((dataImg) => {
       console.log(dataImg.photos);
 
-      // DOM MANIPULATION
+      //****************************************** DOM MANIPULATION *****************************************************
 
-      loadImages.innerHTML = dataImg.photos
+      rowCards.innerHTML = dataImg.photos
         .map(
           (img) => `
-        <img src="${img.src.original}" class="img-fluid img-thumbnail mt-1" alt="...">
+      <div class="col-md-4">
+      <div class="card mb-4 shadow-sm">
+      <div class="card" style="height: 100%">
+          <img src="${img.src.original}" class="img-fluid img-thumbnail mt-1" alt="...">
+          <div class="card-body" style="height: 100%">
+          <h5 class="card-title">${img.alt}</h5>
+          <button class="btn btn-primary btn-modal" data-toggle="modal" data-target="#exampleModal" onclick="changeImg('${img.src.small}')">View</button>
+          <button class="btn btn-primary btn-hide">Hide</button>
+        </div>
+      <div class="card-footer" style="height: 100%">
+        <small class="text-muted">${img.id}</small>
+      </div>
+    </div>
+    </div>
+    </div>
         `
         )
         .join("");
+
+      //************************************* BUTTON HIDE ****************************************************************
+      const btnHide = document.querySelectorAll(".btn-hide");
+
+      btnHide.forEach((button) => {
+        button.addEventListener("click", () => {
+          console.log(button);
+          button.closest(".col-md-4").classList.add("d-none");
+        });
+      });
     })
     .catch((error) => alert("Invalid Page ", error));
 });
 
-// Second Button Load Images
+//************************************** Second Button Load Images ***************************************************
 
 const loadSecondImages = document.querySelector("#loadSecondaryImages");
 
@@ -43,49 +68,42 @@ loadSecondImages.addEventListener("click", () => {
     .then((dataImg) => {
       console.log(dataImg.photos);
 
-      loadSecondImages.innerHTML = dataImg.photos.map(
-        (img) => `
-        <img src="${img.src.original}" class="img-fluid img-thumbnail mt-1" alt="...">
+      rowCards.innerHTML = dataImg.photos
+        .map(
+          (img) => `
+        <div class="col-md-4">
+        <div class="card mb-4 shadow-sm">
+        <div class="card" style="height: 100%">
+            <img src="${img.src.original}" class="img-fluid img-thumbnail mt-1" alt="...">
+            <div class="card-body" style="height: 100%">
+            <h5 class="card-title">${img.alt}</h5>
+            <button class="btn btn-primary btn-modal" data-toggle="modal" data-target="#exampleModal" onclick="changeImg('${img.src.small}')">View</button>
+            <button class="btn btn-primary btn-hide">Hide</button>
+          </div>
+        <div class="card-footer" style="height: 100%">
+          <small class="text-muted">${img.id}</small>
+        </div>
+      </div>
+      </div>
+      </div>
         `
-      );
+        )
+        .join("");
+
+      //********************************************* HIDE BUTTON ****************************************************************
+
+      const btnHide = document.querySelectorAll(".btn-hide");
+
+      btnHide.forEach((button) => {
+        button.addEventListener("click", () => {
+          button.closest(".col-md-4").classList.add("d-none");
+        });
+      });
     })
     .catch((error) => alert("Invalid Page ", error));
 });
 
-// HIDE BUTTON
-
-// const btnHide = document.querySelectorAll(".btn-hide")
-
-// btnHide.addEventListener("click", () => {
-//     const svg = document.querySelectorAll("rect")
-//     console.log(svg)
-//     svg.forEach(element => {
-//         element.classList.add("d-none");
-//     });
-// })
-
-const small = document.querySelectorAll("small");
-
-const replace9mins = () => {
-  const url = "https://api.pexels.com/v1/search?query=cars";
-
-  fetch(url, {
-    headers: {
-      Authorization: password,
-    },
-  })
-    .then((responseImg) => responseImg.json())
-    .then((dataImg) => {
-      small.forEach((element) => {
-        element.innerHTML = dataImg.photos.map(
-          (obj) => `
-            <small class="text-muted">${obj.id}<small>
-        `
-        );
-      });
-    })
-    .catch((error) => alert("Invalid Page ", error));
-};
+//********************************************* INPUT SEARCH ****************************************************************
 
 const search = (event) => {
   const url = "https://api.pexels.com/v1/search?query=cars";
@@ -132,6 +150,8 @@ const search = (event) => {
     .catch((error) => alert("Invalid Page ", error));
 };
 
+//*************************************************** CAROUSEL ************************************************************
+
 const carouselImg = () => {
   const carouselImg = document.querySelector(".carousel");
   const url = "https://api.pexels.com/v1/search?query=trees";
@@ -143,44 +163,131 @@ const carouselImg = () => {
   })
     .then((responseImg) => responseImg.json())
     .then((dataImg) => {
-      console.log(dataImg.photos);
+      // carousel innerHTML = // your carousel html
 
-      carouselImg.innerHTML = dataImg.photos.map(
-        (img) => `
-      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-      <ol class="carousel-indicators">
+      // const innerCarousel = carousel.querySelector('.carousel-inner')
+
+      carouselImg.innerHTML = `
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
         <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
         <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
         <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-      </ol>
-      <div class="carousel-inner">
-        <div class="carousel-item active">
-          <img src="${img.src.original}" class="d-block w-100" alt="...">
-        </div>
-        <div class="carousel-item">
-          <img src="${img.src.original[img]}" class="d-block w-100" alt="...">
-        </div>
-        <div class="carousel-item">
-          <img src="${img.src.original[img]}" class="d-block w-100" alt="...">
-        </div>
-        </div>
-  <button class="carousel-control-prev" type="button" data-target="#carouselExampleIndicators" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-target="#carouselExampleIndicators" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </button>
+    </ol>
+    <div class="carousel-inner">
+    </div>
+    <button class="carousel-control-prev" type="button" data-target="#carouselExampleIndicators" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-target="#carouselExampleIndicators" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </button>
 </div>
-        `
-      ).join("")
+        `;
+
+      const carouselInner = document.querySelector(".carousel-inner");
+
+      carouselInner.innerHTML = dataImg.photos
+        .map(
+          (img) => `
+          <div class="carousel-item">
+            <img src="${img.src.medium}" class="d-block w-100" alt="...">
+          </div>
+          `
+        )
+        .join("");
+      const carouselItem = document.querySelector(".carousel-item");
+
+      carouselItem.classList.add("active");
     })
+
     .catch((error) => alert("Invalid Page ", error));
 };
 
+//************************************* BUTTON VIEW ****************************************************************
+
+const changeImg = (img) => {
+  const imgInModal = document.querySelector("#imgModal")
+
+  imgInModal.src= img;
+}
+
+//************************************* DISPLAY URL ****************************************************************
+
+const displayUrl = () => {
+  const url = "https://api.pexels.com/v1/search?query=trees";
+  const url2 = "https://api.pexels.com/v1/search?query=computers";
+  const url3 = "https://api.pexels.com/v1/search?query=cars"
+
+  // Tress
+  fetch(url, {
+    headers: {
+      Authorization: password,
+    },
+  })
+    .then((responseUrl) => responseUrl.json())
+    .then((dataImgUrl) => {
+
+      dataImgUrl.photos.map((img) => {
+        console.log(img.url)
+      })
+    })
+
+    .catch((error) => alert("Invalid Page ", error));
+
+    // Computers
+    fetch(url2, {
+      headers: {
+        Authorization: password,
+      },
+    })
+      .then((responseUrl) => responseUrl.json())
+      .then((dataImgUrl) => {
+  
+        dataImgUrl.photos.map((img) => {
+          console.log(img.url)
+        })
+      })
+  
+      .catch((error) => alert("Invalid Page ", error));
+  
+      // Cars
+      fetch(url3, {
+        headers: {
+          Authorization: password,
+        },
+      })
+        .then((responseUrl) => responseUrl.json())
+        .then((dataImgUrl) => {
+          dataImgUrl.photos.map((img) => {
+            console.log([img.url])
+          })
+        })
+    
+        .catch((error) => alert("Invalid Page ", error));
+}
+
+const filterAuthor = (favorite) => {
+  const url = "https://api.pexels.com/v1/search?query=computers"
+  fetch(url, {
+    headers: {
+      Authorization: password,
+    },
+  })
+    .then((responseUrl) => responseUrl.json())
+    .then((dataImgUrl) => {
+      console.log(dataImgUrl.photos.filter((img) => img.photographer.toLowerCase() === favorite.toLowerCase()))
+    })
+
+    .catch((error) => alert("Invalid Page ", error));
+
+}
+
+
+
 window.onload = () => {
-  replace9mins();
   document.querySelector("input").addEventListener("change", search);
   carouselImg();
 };
