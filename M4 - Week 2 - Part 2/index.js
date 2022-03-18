87965dd748d1343b76f3d0f3b056885f965b9ef3
Renan Bezerra <renanbezerraenes@icloud.com>
2022-03-18 21:42:25 +0100

    loadBooks function done, with onclick on the button...

--- M4 - Week 2 - Part 2/index.js
+++ M4 - Week 2 - Part 2/index.js
@@ -1,23 +1,29 @@
-const containerDiv = document.querySelector(".displayCards")
-const url = "https://striveschool-api.herokuapp.com/books"
+const containerDiv = document.querySelector(".displayCards");
+const url = "https://striveschool-api.herokuapp.com/books";
+let books = []
+let cart =[]
 
+
+// ************************************************** LoadBooks **********************************************************
 const loadBooks = () => {
-
-fetch(url)
-.then(responseBooks => responseBooks.json())
-.then(booksData => {
-
-    containerDiv.innerHTML = booksData.map( (books) => `
-    <div class="row row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-4" style="color: black">
-    <div class="col-md-4">
+  fetch(url)
+    .then((responseBooks) => responseBooks.json())
+    .then((booksData) => {
+      books = booksData
+      containerDiv.innerHTML = booksData
+        .map(
+          (books) => `
+    <div class="col-md-4" id="${books.asin}">
     <div class="card mb-4 shadow-sm">
     <div class="card" style="height: 100%">
-        <img src="${books.img}" class="img-fluid img-thumbnail" alt="..." style="height: 100%">
+        <img src="${
+          books.img
+        }" class="img-fluid img-thumbnail" alt="..." style="height: 100%">
         <div class="card-body" style="height: 100%">
         <h5 class="card-title">${books.title}</h5>
-        <p>${books.price}</p> 
-        <button class="btn btn-primary btn-modal" data-toggle="modal" data-target="#exampleModal">View</button>
-        <button class="btn btn-primary btn-skip">Skip</button>
+        <p><scan>&#8364</scan>${books.price}</p> 
+        <button onclick="addToCart('${books.asin}')" class="btn btn-primary border-0 btn-add" style="background-color: #0D101A; border-radius: 50px;">Add Cart</button>
+        <button class="btn btn-primary border-0 btn-skip" style="background-color: #0D101A; border-radius: 50px;">Skip</button>
       </div>
     <div class="card-footer" style="height: 100%">
       <small class="text-muted">Category: ${books.category.toUpperCase()}</small>
@@ -25,56 +31,90 @@ fetch(url)
   </div>
   </div>
   </div>
+        `
+        )
+        .join(" ");
+
+      const btnSkip = document.querySelectorAll(".btn-skip");
+
+      btnSkip.forEach((buttonSkip) => {
+        buttonSkip.addEventListener("click", () => {
+          buttonSkip.closest(".col-md-4").remove();
+        });
+      });  
+    })
+    .catch((error) => console.log(error));
+};
+
+function addToCart(asin){
+  
+  const bookToAdd = books.find(book => book.asin === asin)
+  cart.push(bookToAdd)
+
+  const card = document.getElementById(asin)
+  
+  console.log(cart)
+
+}
+
+
+// ************************************************** INPUT SEARCH **********************************************************
+const inputSearch = (bookValue) => {
+  const inputSearchBooks = document.querySelector(".displayBooks");
+  const inputValue = bookValue.target.value;
+
+  fetch(url)
+    .then((responseBooks) => responseBooks.json())
+    .then((booksData) => {
+      inputSearchBooks.innerHTML = booksData
+        .filter((books) =>
+          books.title.toLowerCase().includes(inputValue.toLowerCase())
+        )
+        .map(
+          (books) => `
+    <div class="col-md-4">
+    <div class="card mb-4 shadow-sm">
+    <div class="card" style="height: 100%">
+        <img src="${
+          books.img
+        }" class="img-fluid img-thumbnail" alt="..." style="height: 100%">
+        <div class="card-body" style="height: 100%">
+        <h5 class="card-title">${books.title}</h5>
+        <p>$${books.price}</p> 
+        <button class="btn btn-primary border-0 btn-add" style="background-color: #0D101A; border-radius: 50px;">Add Cart</button>
+        <button class="btn btn-primary border-0 btn-skip" style="background-color: #0D101A; border-radius: 50px;">Skip</button>
+      </div>
+    <div class="card-footer" style="height: 100%">
+      <small class="text-muted">Category: ${books.category.toUpperCase()}</small>
+    </div>
+  </div>
+  </div>
   </div>
         `
-    ).join(" ")
+        )
+        .join(" ");
 
-    const btnSkip = document.querySelectorAll(".btn-skip");
+      const btnSkip = document.querySelectorAll(".btn-skip");
 
-        btnSkip.forEach((buttonSkip) => {
-            buttonSkip.addEventListener("click", () => {
-                buttonSkip.closest(".col-md-4").remove();
+      btnSkip.forEach((buttonSkip) => {
+        buttonSkip.addEventListener("click", () => {
+          buttonSkip.closest(".col-md-4").remove();
         });
       });
-})
-.catch(error => console.log(error))
-}
 
-const inputSearch = (bookValue) => {
-    const inputSearchBooks = document.querySelector(".displayBooks")
-    const inputValue = bookValue.target.value
+      const addButtons = document.querySelectorAll(".btn-add");
 
-    fetch(url)
-    .then(responseBooks => responseBooks.json())
-    .then(booksData => {
-
-        inputSearchBooks.innerHTML = booksData.filter(books => books.title.toLowerCase().includes(inputValue.toLowerCase()))
-    .map( (books) => `
-    <div class="row row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-4" style="color: black">
-    <div class="col-md-4">
-    <div class="card mb-4 shadow-sm">
-    <div class="card" style="height: 100%">
-        <img src="${books.img}" class="img-fluid img-thumbnail" alt="..." style="height: 100%">
-        <div class="card-body" style="height: 100%">
-        <h5 class="card-title">${books.title}</h5>
-        <p>${books.price}</p> 
-        <button class="btn btn-primary btn-modal" data-toggle="modal" data-target="#exampleModal">View</button>
-        <button class="btn btn-primary btn-skip">Skip</button>
-      </div>
-    <div class="card-footer" style="height: 100%">
-      <small class="text-muted">Category: ${books.category.toUpperCase()}</small>
-    </div>
-  </div>
-  </div>
-  </div>
-  </div>
-        `
-    ).join(" ")
+      addButtons.forEach((addButtonCart) => {
+        addButtonCart.addEventListener("click", () => {
+          console.log("BUTTON WORK");
+        });
+      });
     })
-    .catch(error => console.log(error))
-}
+    .catch((error) => console.log(error));
+};
+
 
 window.onload = () => {
-    loadBooks()
-    document.querySelector("#inputSearchBooks").addEventListener("change", inputSearch)
-}
+  loadBooks();
+  document.querySelector("#inputSearchBooks").addEventListener("change", inputSearch);
+};
