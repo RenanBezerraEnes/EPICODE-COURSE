8b6357e23752787a3871a99f48ed734aef0431a3
Renan Bezerra <renanbezerraenes@icloud.com>
2022-03-29 20:51:35 +0200

    Backoffice.js and index.js done.
    On Index.js I am creating a card but the API don't have nothing so nothing is createad and on backoffice.js I am posting a object in the API, so as soon is posted, index.js will generated a card and display it on index.html

--- M4 - Week 4 - Part 1/backoffice.js
+++ M4 - Week 4 - Part 1/backoffice.js
@@ -1,27 +1,45 @@
-const getProductsData = async () => {
+const getNewProducts = async (event) => {
     
+
+    event.preventDefault()
+
+
     const createNewProducts = {
-        
+        name: document.querySelector("#name-input").value,
+        description: document.querySelector("#description-input").value,
+        brand: document.querySelector("#brand-input").value,
+        imageUrl: document.querySelector("#image-input").value,
+        price: document.querySelector("#price-input").value,
     }
 
+    console.log(createNewProducts)
 
     
     try {
-        const myProducts = fetch("https://striveschool-api.herokuapp.com/api/product/", {
+        const myProducts = await fetch("https://striveschool-api.herokuapp.com/api/product/", {
             method: 'POST',
-            body: JSON.stringify(myNewProducts),
+            body: JSON.stringify(createNewProducts),
             headers: {
-            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjQyMWRiNGRhMTNhZjAwMTUyYzFjNWQiLCJpYXQiOjE2NDg1MDAxNDksImV4cCI6MTY0OTcwOTc0OX0.04_ArG8vkOKBaC7uqVG554vt1Xm8JTqqw0vSCBx2QnU"
-        }
-        })
+            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjQyMWRiNGRhMTNhZjAwMTUyYzFjNWQiLCJpYXQiOjE2NDg1MDAxNDksImV4cCI6MTY0OTcwOTc0OX0.04_ArG8vkOKBaC7uqVG554vt1Xm8JTqqw0vSCBx2QnU",
+            'Content-type':'application/json',
+        },
+      }
+    )
 
-        console.log(myProducts)
+    if (myProducts.ok) {
+        
+        alert("Application Done")
+
+        document.querySelector("#name-input").value = ''
+        document.querySelector("#description-input").value = ''
+        document.querySelector("#brand-inpu").value = ''
+        document.querySelector("#image-input").value = ''
+        document.querySelector("#price-input").value = ''
+    } else {
+        alert("We have a problem, Try Again")
+    }
 
     } catch (error) {
-        alert("ERROR", error)
+        console.log(error)
     }
-}
-
-window.onload = async () => {
-    
 }
--- M4 - Week 4 - Part 1/index.js
+++ M4 - Week 4 - Part 1/index.js
@@ -1,6 +1,6 @@
 const getData = async () => {
     try {
-        const myProducts = fetch("https://striveschool-api.herokuapp.com/api/product/", {
+        const myProducts = await fetch("https://striveschool-api.herokuapp.com/api/product/", {
             headers: {
             "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjQyMWRiNGRhMTNhZjAwMTUyYzFjNWQiLCJpYXQiOjE2NDg1MDAxNDksImV4cCI6MTY0OTcwOTc0OX0.04_ArG8vkOKBaC7uqVG554vt1Xm8JTqqw0vSCBx2QnU"
         }
@@ -14,7 +14,7 @@ const getData = async () => {
             console.log("Something went wrong")
         }
     } catch (error) {
-        alert(error)
+        console.log(error)
     }
 }
 
@@ -27,6 +27,7 @@ const createProducts = (products) => {
     <div class="card-body">
       <h5 class="card-title">${products.brand}</h5>
       <p class="card-text">${products.name}</p>
+      <p class="card-text">${products.description}</p>
       <p class="card-text">${products.price}</p>
     </div>
   </div>
@@ -35,6 +36,6 @@ const createProducts = (products) => {
 
 window.onload = async () => {
    let products = await getData()
-   console.log("I have been read", products)
+   console.log("done", products)
    createProducts(products)
 }
