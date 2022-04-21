d77501d8713bd51983cd2e37d79a5d29bd9aa664
Renan Bezerra <renanbezerraenes@icloud.com>
2022-04-21 22:22:15 +0200

    Update on CommentArea, for more information read Description.
    1 - I transferred the fetch from SingleBook to CommenArea.
    2 - I also transferred the state Comment[ ]
    3- I change the Ul, as now it taking the comments from the state and not from the props.

--- M5 - Week 2 - Part 1/module-five-week-two-part-one/src/components/CommentArea.jsx
+++ M5 - Week 2 - Part 1/module-five-week-two-part-one/src/components/CommentArea.jsx
@@ -3,7 +3,11 @@ import AddComment from "./AddComment";
 import CommentList from './CommentsList'
 
 
-export default class CommentArea extends Component {
+class CommentArea extends Component {
+    state = {
+        comments: [],
+    }
+
     render () {
         return (
             
@@ -12,7 +16,7 @@ export default class CommentArea extends Component {
             <AddComment elementId={this.props.asin}/>
             
             <ul>
-                {this.props.comments &&  this.props.comments.map(comment => <CommentList  comment={comment} />)}
+                {this.state.comments &&  this.state.comments.map(comment => <CommentList  comment={comment} />)}
                 {/* {this.props.comments && this.props.comments.map(comment => {
                     console.log('oia eu aqui')
                return <CommentList comment={comment} />
@@ -24,6 +28,23 @@ export default class CommentArea extends Component {
             </>
         )
     }
+
+    componentDidMount = async () => {
+    
+        const response = await fetch ("https://striveschool-api.herokuapp.com/api/comments/" + this.props.asin, {
+        headers: {
+          "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjQyMWRiNGRhMTNhZjAwMTUyYzFjNWQiLCJpYXQiOjE2NTAwMzQ2MDcsImV4cCI6MTY1MTI0NDIwN30.q8-1MZ_TDzIXmHqj4QIMnHVpGC0L_YPc-Az587i8PVQ",
+          'Content-type': 'application/json',
+        }
+      })
+      const APIcomments = await response.json();
+      this.setState({
+        comments: APIcomments,
+      })
+      
+    }
     
 }
+
+export default CommentArea
 
