66e86565c388293778364018f55ece2c4e93129c
Renan Bezerra <renanbezerraenes@icloud.com>
2022-04-22 14:55:53 +0200

    Elevate State, read Description for more information.
    1 - I elevate the state of selected from SingleBook to BookList.
    2 - I made some conditions with ternary operator so the function, border and commentArea will show at the right time.

--- M5 - Week 2 - Part 1/module-five-week-two-part-one/src/components/BookList.jsx
+++ M5 - Week 2 - Part 1/module-five-week-two-part-one/src/components/BookList.jsx
@@ -1,42 +1,51 @@
 import { Component, useState } from "react";
-import { Container, Row, Col, FormControl } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import SingleBook from "./SingleBook";
 import fantasy from "../data/fantasy.json";
 
 class BookList extends Component {
   state = {
       book: fantasy,
-      search:"",
+      selected: "",
   }
 
-  
+  selectedBook = (newState) => {
+    this.setState({
+      selected: newState,
+    })
+  }
 
   render() {
+    console.log(this.state.book, "test")
     // const [load, setLoad] = useState(true) only for funcions
 
     return (
       <Container>
-        <FormControl
+        {/* I removed the FormControl from this component and I applied it to the NavBar Component, but for accesing the filter and map
+        I elevate the state for the nearest parent of both Components and with props and one function I can assign this props to the filter and map
+        and also to the values on the new FormControl on 
+        */}
+
+        {/* <FormControl
           placeholder="Search"
-          value={this.state.search}
+          value={this.props.search}
           onChange={(e) => {
-              this.setState({ search: e.currentTarget.value.toLowerCase() })
-          }}
-        />
-          
+            this.props.searchNavBarInput({ search: e.currentTarget.value.toLowerCase() })
+        // }}
+          // onChange={(e) => {
+          //     this.setState({ search: e.currentTarget.value.toLowerCase() })
+          // }}
+        /> */}
           <Row>
           {this.state.book 
-          .filter(book => book.title.toLowerCase().indexOf(this.state.search) !==-1 || book.category.toLowerCase().indexOf(this.state.search) !==-1)
+          .filter(book => book.title.toLowerCase().indexOf(this.props.search) !==-1 || book.category.toLowerCase().indexOf(this.props.search) !==-1)
           .map((book) => (
             <Col xs={12} md={3} className="my-3" key={book.asin}>
-              <SingleBook book={book} />
+              <SingleBook book={book} selected={this.state.selected} selectedBook={this.selectedBook}/>
             </Col>
           ))
           }
         </Row>
-        
-
-    
       </Container>
     );
   }
