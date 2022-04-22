52dd074ed491809f84c7b152ec1969ac3a915b23
Renan Bezerra <renanbezerraenes@icloud.com>
2022-04-22 22:41:56 +0200

    BookList is finished(Read Description)
    1- I changed the select state for null, so when the page loads I won't have any problems.
    
    2 - I added CommentArea Component to this Component.

--- M5 - Week 2 - Part 1/module-five-week-two-part-one/src/components/BookList.jsx
+++ M5 - Week 2 - Part 1/module-five-week-two-part-one/src/components/BookList.jsx
@@ -2,11 +2,12 @@ import { Component, useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import SingleBook from "./SingleBook";
 import fantasy from "../data/fantasy.json";
+import CommentArea from "./CommentArea";
 
 class BookList extends Component {
   state = {
       book: fantasy,
-      selected: "",
+      selected: null,
   }
 
   selectedBook = (newState) => {
@@ -20,7 +21,7 @@ class BookList extends Component {
     // const [load, setLoad] = useState(true) only for funcions
 
     return (
-      <Container>
+      <Container className="d-flex">
         {/* I removed the FormControl from this component and I applied it to the NavBar Component, but for accesing the filter and map
         I elevate the state for the nearest parent of both Components and with props and one function I can assign this props to the filter and map
         and also to the values on the new FormControl on 
@@ -45,6 +46,12 @@ class BookList extends Component {
             </Col>
           ))
           }
+        </Row>
+
+        <Row  className="ml-5 mt-3">
+        <Col xs={12}>
+          <CommentArea selected={this.state.selected}/>
+          </Col>
         </Row>
       </Container>
     );
