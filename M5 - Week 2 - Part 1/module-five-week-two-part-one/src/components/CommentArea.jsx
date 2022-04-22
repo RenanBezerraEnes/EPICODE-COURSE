f1e60eab435cb199cddd137e5e5b6cd7b86eac00
Renan Bezerra <renanbezerraenes@icloud.com>
2022-04-22 22:43:34 +0200

    CommentArea finished(Read Description)
    1 - I added componentDidMount
    2 - I added componentDidUpdate
    3 - I changed the getCommentaries fetch and applied to the link the this.props.selected(book asin).

--- M5 - Week 2 - Part 1/module-five-week-two-part-one/src/components/CommentArea.jsx
+++ M5 - Week 2 - Part 1/module-five-week-two-part-one/src/components/CommentArea.jsx
@@ -1,6 +1,7 @@
 import { Component } from "react";
 import AddComment from "./AddComment";
 import CommentList from './CommentsList'
+import { ListGroup } from "react-bootstrap";
 
 
 class CommentArea extends Component {
@@ -9,13 +10,14 @@ class CommentArea extends Component {
     }
 
     render () {
+      console.log(this.props.selected, "LOOK ME")
         return (
             
             <>
-            <h5>FeedBacks:</h5>
-            <AddComment elementId={this.props.asin}/>
             
-            <ul>
+            <AddComment elementId={this.props.selected}/>
+            <h5 className="text-center mt-3">FeedBacks:</h5>
+            <ListGroup>
                 {this.state.comments &&  this.state.comments.map(comment => <CommentList  comment={comment} />)}
                 {/* {this.props.comments && this.props.comments.map(comment => {
                     console.log('oia eu aqui')
@@ -24,14 +26,20 @@ class CommentArea extends Component {
                 {/* It is really important to console.log everything to understand, and in the second case using map with curly braces, make possible to console.log
                 otherwise it's not possible to console.log because it will return automatically
                 */}
-            </ul>
+            </ListGroup>
             </>
         )
     }
 
-    componentDidMount = async () => {
+
+    componentDidMount =  () => {
+      this.getCommentaries()
+    }
+
+
+    getCommentaries = async () => {
     
-        const response = await fetch ("https://striveschool-api.herokuapp.com/api/comments/" + this.props.asin, {
+        const response = await fetch ("https://striveschool-api.herokuapp.com/api/comments/" + this.props.selected, {
         headers: {
           "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjQyMWRiNGRhMTNhZjAwMTUyYzFjNWQiLCJpYXQiOjE2NTAwMzQ2MDcsImV4cCI6MTY1MTI0NDIwN30.q8-1MZ_TDzIXmHqj4QIMnHVpGC0L_YPc-Az587i8PVQ",
           'Content-type': 'application/json',
@@ -41,7 +49,14 @@ class CommentArea extends Component {
       this.setState({
         comments: APIcomments,
       })
-      
+    }
+
+
+    componentDidUpdate = (prevProps, prevState) => {
+  
+      if (prevProps.selected !== this.props.selected) {
+        this.getCommentaries()
+      }
     }
     
 }
