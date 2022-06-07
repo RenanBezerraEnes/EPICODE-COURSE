e11e3ee0c36d00614d094050ea219e0a5950ff77
Renan Bezerra <renanbezerraenes@icloud.com>
2022-06-07 14:49:49 +0200

    404 Not Found + CSS

--- M5 - Week 4 - Part 1/module-five-last-week-part-one/src/components/NotFound.jsx
+++ M5 - Week 4 - Part 1/module-five-last-week-part-one/src/components/NotFound.jsx
@@ -1,12 +1,23 @@
 import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import "./NotFoundStyle.css";
 
 export default function NotFound() {
 	return (
-		<div className="d-flex flex-column justify-content-center">
-			<img
-				src="https://www.wigital.de/img/1104/1000/500/1/fehler-404-page-not-found.jpg"
-				alt=""
-			/>
-		</div>
+		<Container fluid className="NotFoundBackGround">
+			<Row className="m-0">
+				<Col
+					xs={12}
+					style={{ height: "100%" }}
+					className="d-flex justify-content-center align-items-center p-0 img-notFound"
+				>
+					<img
+						style={{ width: "100%", height: "100%" }}
+						src="https://www.wigital.de/img/1104/1000/500/1/fehler-404-page-not-found.jpg"
+						alt="Not Found"
+					/>
+				</Col>
+			</Row>
+		</Container>
 	);
 }
