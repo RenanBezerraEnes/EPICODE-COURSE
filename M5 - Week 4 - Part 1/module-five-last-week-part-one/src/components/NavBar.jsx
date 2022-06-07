b6884699306f77b7df1195d86f1196bc5f3bb053
Renan Bezerra <renanbezerraenes@icloud.com>
2022-06-07 14:49:38 +0200

    M5 - Week 4 - Navbar

--- M5 - Week 4 - Part 1/module-five-last-week-part-one/src/components/NavBar.jsx
+++ M5 - Week 4 - Part 1/module-five-last-week-part-one/src/components/NavBar.jsx
@@ -1,9 +1,21 @@
 import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const NavBar = ({ search, setSearch }) => {
+	const location = useLocation();
+
+	console.log("LOCATION", location);
+
 	return (
-		<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+		<Navbar
+			collapseOnSelect
+			expand="lg"
+			variant="dark"
+			style={{
+				background:
+					"linear-gradient(179deg, rgb(138 0 243) 10%, rgb(85 67 233) 50%, rgb(174 103 229) 100%)",
+			}}
+		>
 			<Navbar.Toggle aria-controls="responsive-navbar-nav" />
 			<Navbar.Brand href="#home">
 				<img
@@ -15,13 +27,25 @@ const NavBar = ({ search, setSearch }) => {
 			</Navbar.Brand>
 			<Navbar.Collapse id="responsive-navbar-nav">
 				<Nav className="mr-auto">
-					<Nav.Link as={Link} to="/">
+					<Nav.Link
+						as={Link}
+						to="/"
+						className={location.pathname === "/" ? "active" : ""}
+					>
 						Home
 					</Nav.Link>
-					<Nav.Link as={Link} to="/about">
+					<Nav.Link
+						as={Link}
+						to="/about"
+						className={location.pathname === "/about" ? "active" : ""}
+					>
 						About
 					</Nav.Link>
-					<Nav.Link as={Link} to="/Browse">
+					<Nav.Link
+						as={Link}
+						to="/Browse"
+						className={location.pathname === "/Browse" ? "active" : ""}
+					>
 						Browse
 					</Nav.Link>
 				</Nav>
